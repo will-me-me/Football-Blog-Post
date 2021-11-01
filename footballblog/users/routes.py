1091796d8124a47fd72faf8b3ad7946df6512cad
willkaria <willykaria777@gmail.com>
2021-11-01 10:31:30 +0300

    fixed bugs on users/routes.py

--- footballblog/users/routes.py
+++ footballblog/users/routes.py
@@ -20,7 +20,7 @@ def register():
         return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password=Bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user=User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
--- footballblog/users/utils.py
+++ footballblog/users/utils.py
@@ -27,13 +27,8 @@ def save_picture(form_picture):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-
-    msg = Message()
-    msg.subject = 'Flask App Password Reset'
-    msg.sender = os.getenv('MAIL_USERNAME')
-    msg.recipients = [user.email]
     msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
+                  sender='noreply@gmail.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
